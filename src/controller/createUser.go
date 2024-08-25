d9d3fa756169091878922b483ce921b762cf5a60
Diogo <diogo.alvesf88@gmail.com>
2024-08-25 04:52:21 -0300

    feat: creating application routes

---
+++ src/controller/createUser.go
@@ -0,0 +1,11 @@
+package controller
+
+import (
+	rest_err "github.com/diogoalvesf/meu-primeiro-crud-go/src/controller/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
+	c.JSON(err.Code, err)
+}
