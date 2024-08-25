3068f9db521525b43c8d37fdda58e5812c2f0f40
Diogo <diogo.alvesf88@gmail.com>
2024-08-25 07:34:29 -0300

    feat: creating user objects

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/diogoalvesf/meu-primeiro-crud-go/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,11 +1,21 @@
 package controller
 
 import (
-	rest_err "github.com/diogoalvesf/meu-primeiro-crud-go/src/controller/configuration/rest_err"
+	"fmt"
+
+	"github.com/diogoalvesf/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/diogoalvesf/meu-primeiro-crud-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
